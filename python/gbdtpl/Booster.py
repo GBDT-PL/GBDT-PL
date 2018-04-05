39a4e363ac176f80c0c5ea76da014a3209641014
GBDT-PL <sysjtucs@gmail.com>
2018-04-04 23:09:37 -0400

    add prediction interface

--- python/gbdtpl/Booster.py
+++ python/gbdtpl/Booster.py
@@ -1,4 +1,5 @@
 from ctypes import *
+import numpy as np
 
 class Booster:
     def __init__(self, params, train_data, test_data):
@@ -13,3 +14,15 @@ class Booster:
 
     def Train(self):
          self.LinearGBMLib.Train(self.booster)
+
+    def __c_pointer_to_numpy_array(self, preds, num_data):
+        numpy_preds = np.zeros(num_data, dtype=np.float64)
+        memmove(numpy_preds.ctypes.data, preds, num_data * numpy_preds.strides[0])
+        return numpy_preds
+
+    def Predict(self, test_data):
+        preds = POINTER(c_double)()
+        num_data = c_int()
+        self.LinearGBMLib.LinearGBMPredict(self.booster, test_data.data_mat, byref(preds), byref(num_data))
+        return self.__c_pointer_to_numpy_array(preds, num_data.value)
+
